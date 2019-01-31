77f140c9ac6c4f035733510ce8fd38468e9fed8e
xiewenqian <wendyxie327@163.com>
2019-01-31 14:14:31 +0800

    NestedScrollView 标题栏收缩

--- babycare/lib/widget/home_page.dart
+++ babycare/lib/widget/home_page.dart
@@ -11,31 +11,50 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
-        appBar: new AppBar(
-          title: new Text(GlobalConfig.appName),
-          centerTitle: true,
-        ),
-        body: new ListView(
-          children: homeBody()
+//        appBar: new AppBar(
+//          title: new Text(GlobalConfig.appName),
+//          centerTitle: true,
+//        ),
+        body: new NestedScrollView(
+          headerSliverBuilder: _sliverAppBarBuilder,
+          body: new ListView(
+            children: homeBody(),
+          ),
         ));
+  }
+
+  List<Widget> _sliverAppBarBuilder(
+      BuildContext context, bool innerBoxIsScrolled) {
+    return <Widget>[
+      SliverAppBar(
+        centerTitle: true,
+        //标题居中
+        expandedHeight: 200.0,
+        //展开高度200
+        floating: false,
+        //不随着滑动隐藏标题
+        pinned: true,
+
+        //固定在顶部
+        flexibleSpace: FlexibleSpaceBar(
+            title: Text("糖糖 6月12天", style: TextStyle(
+              fontSize: 16,
+            ),),
+          background: Image.asset(
+            "images/bg.png",
+            height: 200,
+            width: MediaQuery.of(context).size.width,
+            fit: BoxFit.cover,
+            alignment: Alignment.center,
+          ),
+        ),
+      ),
+    ];
   }
 
   // 首页显示内容
   homeBody() {
     var body = <Widget>[];
-
-    // 图片内容
-    body.add(new Container(
-      margin: EdgeInsets.fromLTRB(0, 0, 0, 16),
-      child: Image.asset(
-        "images/bg.png",
-        height: 200,
-        width: MediaQuery.of(context).size.width,
-        fit: BoxFit.cover,
-        alignment: Alignment.center,
-      ),
-    ));
-
     // 内容
     List titles = ["喂养", "尿片", "睡眠", "身高"];
     for (var i = 0; i < titles.length; i++) {
